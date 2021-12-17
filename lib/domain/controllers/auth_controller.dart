1940d03b53eaafa2ae999d596a4f90873630c2ba
Ofpenagos <92539438+Ofpenagos@users.noreply.github.com>
2021-12-16 22:36:27 -0500

    Add files via upload

---
+++ lib/domain/controllers/auth_controller.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationController extends GetxController {
+  Future<void> login(theEmail, thePassword) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: theEmail, password: thePassword);
+      // ignore: void_checks
+      return Future.value(true);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return Future.error("Usuario no encontrado!");
+      } else if (e.code == 'wrong-password') {
+        return Future.error("Contraseña incorrecta!");
+      } else {
+        return Future.error("Error: ${e.toString()}");
+      }
+    }
+  }
+
+
+Future<void> signUp(email, password) async {
+  try {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    // ignore: void_checks
+    return Future.value(true);
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      return Future.error('La Contraseña no cumple con las politicas!');
+    } else if (e.code == 'email-already-in-use') {
+      return Future.error('El Correo Electronico ya se encuentra registrado!');
+    }
+  }
+}
+
+Future<void> logOut() async {
+  try {
+    await FirebaseAuth.instance.signOut();
+  } catch (e) {
+    return Future.error(e.toString());
+  }
+}
+  String userEmail() {
+    String email = FirebaseAuth.instance.currentUser!.email ?? "a@a.com";
+    return email;
+}
+  String getUid() {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    return uid;
+  }
+}
